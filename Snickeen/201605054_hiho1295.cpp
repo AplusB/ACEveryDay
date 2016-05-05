a4b6414e1bd4caa23084f5eb2e65c77d5a239158
Snickeen <ff@snickeen.com>
2016-05-05 21:36:53 +0800

    Update 201605054_hiho1295.cpp

--- Snickeen/201605054_hiho1295.cpp
+++ Snickeen/201605054_hiho1295.cpp
@@ -4,16 +4,16 @@
 int n,i,j;
 int pri[N],pn[N/10];
 int main(){
-	for(i=2;i<N;i++){
-		if(!pri[i]) pn[++pn[0]]=i;
-		for(j=1;(j<=pn[0])&&(pn[j]*i<=N);j++){
-			pri[pn[j]*i]=1;
-			if(i%pn[j]==0)break;
-		}
+    for(i=2;i<N;i++){
+        if(!pri[i]) pn[++pn[0]]=i;
+        for(j=1;(j<=pn[0])&&(pn[j]*i<=N);j++){
+            pri[pn[j]*i]=1;
+            if(i%pn[j]==0)break;
+        }
 	}
 	pri[0]=pri[1]=1;
 	for(i=2;i<N;i++)pri[i]=pri[i]+pri[i-1];
 	scanf("%d",&n);
 	printf("%d\n",n-pri[n]);
-	return 0;
+    return 0;
 }
