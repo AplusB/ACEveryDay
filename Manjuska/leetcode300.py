52b97172eeef4e81a684e457f4927d1d9ea5a3c0
Zheao.Li <lizheao940510@gmail.com>
2016-05-08 21:18:37 +0800

    leetcode300.py

--- Manjuska/leetcode300.py
+++ Manjuska/leetcode300.py
@@ -7,3 +7,32 @@
 @file: leetcode300.py
 @time: 21:03
 """
+
+
+def lengthOfLIS(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    if nums == None or len(nums) < 1:
+        return 0
+    dp=[]
+    dp=[]
+    dp.append(1)
+    max1 = 1
+    for i in range(0, len(nums)):
+        dp.append(1)
+        #print('i=',i)
+        #print('dp[i]=',dp[i])
+        for j in range(0, i):
+            print(dp[j])
+            if (nums[j] < nums[i]):
+                dp[i] = max(dp[i], dp[j] + 1)
+                print(dp[i])
+        max1 = max(max1, dp[i])
+        print(max1)
+    return max1
+if __name__=='__main__':
+    test=[-2,-1]
+    temp=lengthOfLIS(test)
+    print(temp)
