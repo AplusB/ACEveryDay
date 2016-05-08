452852b862d4eb558f9cc9dbdf0daeb1d5aeaa6e
QAsQ <317661924@qq.com>
2016-05-08 18:31:18 +0800

    随手把模板丢了上去

---
+++ QAsQ/template/Math/inv.cpp
@@ -0,0 +1,15 @@
+﻿//ax = 1(mod n)
+long long mod_reverse(long long a,long long n)
+{
+    long long x,y;
+    long long d=extend_gcd(a,n,x,y);
+    if(d==1) return (x%n+n)%n;
+    else return -1;
+}
+
+//这个只能求 a < m 的情况，而且必须保证 a 和 m 互质
+long long inv(long long a,long long m)
+{
+    if(a == 1)return 1;
+    return inv(m%a,m)*(m-m/a)%m;
+}
---
+++ QAsQ/template/Math/powMartix.cpp
@@ -0,0 +1,48 @@
+const int N=2;
+const int MOD = 1000000007;
+#define LL long long
+
+struct martix{
+    LL num[N][N];
+    void tz(){
+        memset(num,0,sizeof(num));
+    }
+    void to(){
+        this->tz();
+        for(int i=0;i<N;i++)
+            num[i][i]=1;
+    }
+    void out(){
+        puts("-------");
+        for(int i=0;i<N;i++)
+            for(int j=0;j<N;j++)
+                printf(j<N-1?"%d ":"%d\n",num[i][j]);
+        puts("=======");
+    }
+};
+
+martix operator * (const martix & A,const martix & B){
+    martix C;
+    C.tz();
+    for(int i=0;i<N;i++)
+        for(int j=0;j<N;j++)
+            for(int k=0;k<N;k++)
+                C.num[i][j]=(C.num[i][j]+A.num[i][k]*B.num[k][j]%MOD)%MOD;
+    return C;
+}
+
+
+martix qpow(martix x,int n)
+{
+    martix res;
+    res.to();
+    while(n>0)
+    {
+        if(n & 1)
+            res=res*x;
+        x=x*x;
+        n >>= 1;
+    }
+    return res;
+}
+
