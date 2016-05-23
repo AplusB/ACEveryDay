21e1f7f3284a60511d23f9a07997e6339001afeb
fsszns <fsszns@163.com>
2016-05-23 19:51:55 +0800

    20160521

--- zy/main.cpp
+++ zy/main.cpp
@@ -1,72 +1,53 @@
 #include <iostream>
 #include <stdio.h>
-#include <cmath>
+#include <math.h>
+#include <vector>
+#include <map>
+#include <string.h>
 using namespace std;
-typedef long long LL;
 
-const int N=1e5+5;
-int n,a[N];
-LL dp[N];
-
-struct Tree
-{
-    int l,r,ans;
-}tr[4*N];
-
-void pushup(int root)
-{
-    if(a[tr[2*root].ans]>a[tr[2*root+1].ans])
-        tr[root].ans=tr[2*root].ans;
-    else tr[root].ans=tr[2*root+1].ans;
-}
-
-void build(int root,int L,int R)
-{
-    tr[root].l=L;
-    tr[root].r=R;
-    if(L==R)
-    {
-        tr[root].ans=L;
-        return ;
-    }
-    int mid=(L+R)>>1;
-    build(2*root,L,mid);
-    build(2*root+1,mid+1,R);
-    pushup(root);
-}
-
-int query(int root,int L,int R)
-{
-    if(L<=tr[root].l&&R>=tr[root].r)return tr[root].ans;
-    int mid=(tr[root].l+tr[root].r)>>1;
-    if(R<=mid)return query(2*root,L,R);
-    else if(L>mid)return query(2*root+1,L,R);
-    else
-    {
-        int fl=query(2*root,L,mid),fr=query(2*root+1,mid+1,R);
-        if(a[fl]>a[fr])return fl;
-        else return fr;
-    }
-}
+typedef long long ll;
+map<int,int> mp;
+vector<int> vec;
 
 int main()
 {
-    scanf("%d",&n);
-    for(int i=1;i<=n-1;i++)
-        scanf("%d",&a[i]);
-    build(1,1,n);
-    LL ans=0;
-    
-    a[n]=n-1;
-    dp[n]=0;
-    for(int i=n-1;i>0;i--)
+    int ca;
+    int cnt = 0;
+    int ans;
+    ll x,m,k,c,temp;
+    scanf("%d",&ca);
+    for(int cc=1;cc<=ca;cc++)
     {
-        int temp=query(1,i+1,a[i]);
-        dp[i]=dp[temp]+(LL)(n-i-(a[i]-temp));
-        ans+=dp[i];
+        temp=0;
+        ans=-1;
+        mp.clear();
+        vec.clear();
+        scanf("%lld%lld%lld%lld",&x,&m,&k,&c);
+
+        for(int i = 1; i <= m; ++i)
+        {
+            temp=temp*10+x;
+            temp%=k;
+            if(mp[temp]!=0)
+            {
+                int len=i-mp[temp];
+                m-=mp[temp]-1;
+                m%=len;
+                if(m==0) ans=vec[vec.size()-1];
+                else ans=vec[mp[temp]+m-1];
+                break;
+            }
+            vec.push_back(temp);
+            mp[temp]=i;
+        }
+        
+        if(ans==-1) ans=vec[m-1];
+        printf("Case #%d:\n",cc);
+        
+        if(ans==c)
+            puts("Yes");
+        else puts("No");
     }
-    
-    cout<<ans<<endl;
-    
     return 0;
 }
