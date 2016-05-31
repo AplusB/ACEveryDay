b0e17a4206c8371f53a7ff198584d39ba99db80f
comma01 <442735792@qq.com>
2016-05-31 23:37:01 +0800

    5.31
    Today's work and yesterday's change!

--- wudo/P.cpp
+++ wudo/P.cpp
@@ -1,38 +1,46 @@
 #include <cstdio>
+#include <cstring>
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
 
-const int MAX = 1000 + 3;
-char m[MAX][MAX];
-int dp[MAX][MAX];
- 
-int main()
+const int MAX = 100 + 10;
+int dp[MAX][MAX], s[MAX][MAX];
+int n, k;
+int dx[] = {-1, 0, 0, 1};
+int dy[] = {0, -1, 1, 0};
+
+int dfs(int x, int y)
 {
-    int n;
-    while (scanf("%d", &n), n) {
-        for (int i = 0; i < n; ++i)
-            scanf("%s", m[i]);
-        
-        int ans = 1;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (i == 0 || j == n - 1) {
-                    dp[i][j] = 1;
-                    continue;
+    int maxx = 0, sum, xx, yy;
+    if (!dp[x][y]) {
+        for (int i = 1; i <= k; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                xx = x + dx[j] * i;
+                yy = y + dy[j] * i;
+                if (xx >= 0 && xx < n && yy >= 0 && yy < n && s[xx][yy] > s[x][y]) {
+                    sum = dfs(xx, yy);
+                    maxx = max(maxx, sum);
                 }
-                   
-                int x = i, y = j;
-                while (x >= 0 && y < n && m[x][j] == m[i][y]) {
-                    --x, ++y;
-                }
-                x = i - x;
-                if (x >= dp[i - 1][j + 1] + 1) dp[i][j] = dp[i - 1][j + 1] + 1;
-                else dp[i][j] = x;
-                ans = max(ans, dp[i][j]);
             }
         }
-        printf("%d\n", ans);
+        dp[x][y] = maxx + s[x][y];
+    }
+    return dp[x][y];
+}
+
+int main()
+{
+    while (scanf("%d%d", &n, &k), n != -1, k != -1) {
+        
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                scanf("%d", &s[i][j]);
+            }
+        }
+        memset(dp, 0, sizeof(dp));
+        printf("%d\n", dfs(0,0));
     }
     
     return 0;
