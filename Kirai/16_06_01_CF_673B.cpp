6dcf45ef92511619b40f23c700034a8518496108
Kirai <2014011234@mail.bistu.edu.cn>
2016-06-01 12:24:24 +0800

    Create 16_06_01_CF_673B.cpp

---
+++ Kirai/16_06_01_CF_673B.cpp
@@ -0,0 +1,87 @@
+/*
+━━━━━┒ギリギリ♂ eye！
+┓┏┓┏┓┃キリキリ♂ mind！
+┛┗┛┗┛┃＼○／
+┓┏┓┏┓┃ /
+┛┗┛┗┛┃ノ)
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┃┃┃┃┃┃
+┻┻┻┻┻┻
+*/
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <climits>
+#include <complex>
+#include <fstream>
+#include <cassert>
+#include <cstdio>
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <ctime>
+#include <set>
+#include <map>
+#include <cmath>
+using namespace std;
+#define fr first
+#define sc second
+#define cl clear
+#define BUG puts("here!!!")
+#define W(a) while(a--)
+#define pb(a) push_back(a)
+#define Rlf(a) scanf("%llf", &a);
+#define Rint(a) scanf("%d", &a)
+#define Rll(a) scanf("%I64d", &a)
+#define Rs(a) scanf("%s", a)
+#define Cin(a) cin >> a
+#define FRead() freopen("in", "r", stdin)
+#define FWrite() freopen("out", "w", stdout)
+#define Rep(i, len) for(int i = 0; i < (len); i++)
+#define For(i, a, len) for(int i = (a); i < (len); i++)
+#define Cls(a) memset((a), 0, sizeof(a))
+#define Clr(a, x) memset((a), (x), sizeof(a))
+#define Full(a) memset((a), 0x7f7f, sizeof(a))
+#define lrt rt << 1
+#define rrt rt << 1 | 1
+#define pi 3.14159265359
+#define RT return
+#define lowbit(x) x & (-x)
+#define onenum(x) __builtin_popcount(x)
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ULL;
+typedef pair<int, int> pii;
+typedef pair<string, int> psi;
+typedef map<string, int> msi;
+typedef vector<int> vi;
+typedef vector<LL> vl;
+typedef vector<vl> vvl;
+typedef vector<bool> vb;
+
+const int maxn = 100100;
+int n, m;
+
+int main() {
+	// FRead();
+	int u, v;
+	Rint(n); Rint(m);
+	int l = 1, r = n;
+	Rep(i, m) {
+		Rint(u); Rint(v);
+		if(u > v) swap(u, v);
+		l = max(u, l); r = min(v, r);
+	}
+	if(r - l < 0) printf("0\n");
+	else printf("%d\n", r-l);
+	RT 0;
+}
