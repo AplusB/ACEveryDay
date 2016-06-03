91eb6fd255550882a3799394265ed40c71fcd4c7
wuyiqi <wuyiqi.splay@gmail.com>
2016-06-03 23:34:53 +0800

    water

--- wuyiqi/cf353/A.cpp
+++ wuyiqi/cf353/A.cpp
@@ -1,10 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int sgn(int x)
+{
+        return x > 0 ? 1 : (x < 0 ? -1 : 0);
+}
 int main() {
         int a, b, c;
         cin >> a >> b >> c;
-        if(b==a || c && (b - a) % c == 0 && (b - a) >= 0) {
+        if(b==a || c && (b - a) % c == 0 && sgn(b-a)==sgn(c) ) {
                 cout << "YES";
         } else {
                 cout << "NO";
