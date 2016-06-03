4b5d1b0ee78007bef740067fb3e2f668054ebec3
Kirai <2014011234@mail.bistu.edu.cn>
2016-06-03 13:28:15 +0800

    Create 16_06_03_CF_677C.cpp

---
+++ Kirai/16_06_03_CF_677C.cpp
@@ -0,0 +1,103 @@
+/*
+━━━━━┒ギリギリ♂ eye！
+┓┏┓┏┓┃キリキリ♂ mind！
+┛┗┛┗┛┃＼○／
+┓┏┓┏┓┃ /
+┛┗┛┗┛┃ノ)
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┃┃┃┃┃┃
+┻┻┻┻┻┻
+*/
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <climits>
+#include <complex>
+#include <fstream>
+#include <cassert>
+#include <cstdio>
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <ctime>
+#include <set>
+#include <map>
+#include <cmath>
+using namespace std;
+#define fr first
+#define sc second
+#define cl clear
+#define BUG puts("here!!!")
+#define W(a) while(a--)
+#define pb(a) push_back(a)
+#define Rlf(a) scanf("%lf", &a);
+#define Rint(a) scanf("%d", &a)
+#define Rll(a) scanf("%I64d", &a)
+#define Rs(a) scanf("%s", a)
+#define Cin(a) cin >> a
+#define FRead() freopen("in", "r", stdin)
+#define FWrite() freopen("out", "w", stdout)
+#define Rep(i, len) for(int i = 0; i < (len); i++)
+#define For(i, a, len) for(int i = (a); i < (len); i++)
+#define Cls(a) memset((a), 0, sizeof(a))
+#define Clr(a, x) memset((a), (x), sizeof(a))
+#define Full(a) memset((a), 0x7f7f, sizeof(a))
+#define lrt rt << 1
+#define rrt rt << 1 | 1
+#define pi 3.14159265359
+#define RT return
+#define lowbit(x) x & (-x)
+#define onenum(x) __builtin_popcount(x)
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ULL;
+typedef pair<int, int> pii;
+typedef pair<string, int> psi;
+typedef map<string, int> msi;
+typedef vector<int> vi;
+typedef vector<LL> vl;
+typedef vector<vl> vvl;
+typedef vector<bool> vb;
+
+const int maxn = 100100;
+const int mod = 1000000007;
+int cv[256];
+char s[maxn];
+
+int quickmul(int x, int n) {
+	int ret = 1, t = x;
+	while(t) {
+		if(n & 1) ret = (ret * t) % mod;
+		t = t * t % mod;
+		n >>= 1;
+	}
+	return ret;
+}
+
+int main() {
+	// FRead();
+	Rep(i, 10) cv['0'+i] = i;
+	For(i, 10, 36) cv['A'+i-10] = i;
+	For(i, 36, 62) cv['a'+i-36] = i;
+	cv['-'] = 62; cv['_'] = 63;
+	while(~Rs(s)) {
+		int n = strlen(s);
+		LL ret = 1;
+		Rep(i, n) {
+			int p = cv[s[i]];
+			int t = 6 - __builtin_popcount(p);
+			ret = (ret * quickmul(3, t)) % mod;
+		}
+		printf("%I64d\n", ret);
+	}
+	RT 0;
+}
