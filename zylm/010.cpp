7abf8df74d319244eab40a5853f56f966d141bab
zylm <736953053@qq.com>
2016-06-04 09:32:43 +0800

    Update 010.cpp

--- zylm/010.cpp
+++ zylm/010.cpp
@@ -1,4 +1,4 @@
-//http://codeforces.com/contest/677/problem/B
+//http://codeforces.com/contest/677/problem/B 数据范围
 #include <cstdio>
 #include <cstring>
 #include <cmath>
@@ -16,28 +16,37 @@ int main()
 {
 //	read;
 	std::ios::sync_with_stdio(false);
-	int n,h,k;
-	while(~scanf("%d%d%d",&n,&h,&k))
+	LL n,h,k;
+	while(~scanf("%I64d%I64d%I64d",&n,&h,&k))
 	{
-		int i,j,res=0;
+		int i,j;
+		LL res=0;
 		for(i=0;i<n;i++)
-			scanf("%d",&a[i]);
+			scanf("%I64d",&a[i]);
 		long long tmp=0;
 		for(i=0;i<n;)
 		{
+			int flag=0;
 			while(tmp+a[i]<=h && i<n)
 			{
 				tmp+=(long long)a[i];
 				i++;
+				flag=1;
 			}
-			if(tmp-k<0)tmp=0;
+			if(flag)
+			{
+				res+= tmp/k;
+			    tmp%=k; 
+			}
+			else 
+			{
+				res++;
+				tmp=0;
+		    }
 			 
-			res++;
-		//	cout<<tmp<<endl;
-			
 		}
 		if(tmp>0)res++;
 		
-		printf("%d\n",res);
+		printf("%I64d\n",res);
 	}
 }
