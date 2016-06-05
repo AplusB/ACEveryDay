01908839f6d37d62bf0ef2643fba17546f7b4d7c
Kirai <2014011234@mail.bistu.edu.cn>
2016-06-05 21:10:54 +0800

    Create 16_06_05_HIHO_1309.cpp

---
+++ Kirai/16_06_05_HIHO_1309.cpp
@@ -0,0 +1,94 @@
+/* 
+┓┏┓┏┓┃キリキリ♂ mind！
+┛┗┛┗┛┃＼○／
+┓┏┓┏┓┃ /
+┛┗┛┗┛┃ノ)
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┃┃┃┃┃┃
+┻┻┻┻┻┻
+*/
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <climits>
+#include <complex>
+#include <fstream>
+#include <cassert>
+#include <cstdio>
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <ctime>
+#include <set>
+#include <map>
+#include <cmath>
+using namespace std;
+#define fr first
+#define sc second
+#define cl clear
+#define BUG puts("here!!!")
+#define W(a) while(a--)
+#define pb(a) push_back(a)
+#define Rlf(a) scanf("%lf", &a);
+#define Rint(a) scanf("%d", &a)
+#define Rll(a) scanf("%lld", &a)
+#define Rs(a) scanf("%s", a)
+#define Cin(a) cin >> a
+#define FRead() freopen("in", "r", stdin)
+#define FWrite() freopen("out", "w", stdout)
+#define Rep(i, len) for(int i = 0; i < (len); i++)
+#define For(i, a, len) for(int i = (a); i < (len); i++)
+#define Cls(a) memset((a), 0, sizeof(a))
+#define Clr(a, x) memset((a), (x), sizeof(a))
+#define Full(a) memset((a), 0x7f7f, sizeof(a))
+#define lrt rt << 1
+#define rrt rt << 1 | 1
+#define pi 3.14159265359
+#define RT return
+#define lowbit(x) x & (-x)
+#define onenum(x) __builtin_popcount(x)
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ULL;
+typedef pair<int, int> pii;
+typedef pair<string, int> psi;
+typedef map<string, int> msi;
+typedef vector<int> vi;
+typedef vector<LL> vl;
+typedef vector<vl> vvl;
+typedef vector<bool> vb;
+
+int n;
+LL ret;
+map<LL, LL> cnt;
+map<LL, LL>::iterator it;
+
+int main() {
+	// FRead();
+	LL s, e;
+	while(~Rint(n)) {
+		cnt.cl();
+		Rep(i, n) {
+			cin >> s >> e;
+			cnt[s]++; cnt[e]--;
+			// if(cnt[e] < 0) cnt[e] = 0;
+		}
+		ret = 0;
+		LL cur = 0;
+		for(it = cnt.begin(); it != cnt.end(); it++) {
+			cur += it->sc;
+			ret = max(ret, cur);
+		}
+		cout << ret << endl;
+	}
+	RT 0;
+}
