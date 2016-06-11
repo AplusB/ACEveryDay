21543b7750c521cf80ad27a00cbcfccfb653e80d
wuyiqi <wuyiqi.splay@gmail.com>
2016-06-11 23:38:09 +0800

    shuiti

---
+++ wuyiqi/cf347/A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+        string a, b;
+
+        cin >> a >> b;
+        if(a != b) cout << 1 << endl;
+        else cout << a << endl;
+        return 0;
+}
