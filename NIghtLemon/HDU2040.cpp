cab3d2c87beb8841b407ad158096acf7348877a5
NightLemon <569449980@qq.com>
2016-06-13 23:58:51 +0800

    Update HDU2040.cpp

--- NIghtLemon/HDU2040.cpp
+++ NIghtLemon/HDU2040.cpp
@@ -1,16 +1,17 @@
-//A+B签到
 #include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-	int T,a,b,c,d;
+	int T,a,b,c,d,i;
 	scanf("%d",&T);
 	while(T--){
 		scanf("%d%d",&a,&b);
-		c=d=0;
-		for(int i=1;i<=a/2;i++) if(a%i==0) c+=i;
-		for(int i=1;i<=b/2;i++) if(b%i==0) d+=i;
+		c=d=1;
+		for(i=2;i<=sqrt(a);i++) if(a%i==0) { c+=i; c+=a/i;}
+		if(i*i==a) c+=i;
+		for(i=2;i<=sqrt(b);i++) if(b%i==0) { d+=i; d+=b/i;}
+		if(i*i==b) d+=i;
 		if(c==b && a==d) printf("YES\n");
 		else printf("NO\n");
 	}	
