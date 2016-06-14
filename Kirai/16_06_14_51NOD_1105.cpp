76212848e4d8aceb637b92675c99e620c3b7664b
Kirai <2014011234@mail.bistu.edu.cn>
2016-06-14 14:21:07 +0800

    Create 16_06_14_51NOD_1105.cpp

---
+++ Kirai/16_06_14_51NOD_1105.cpp
@@ -0,0 +1,111 @@
+/*
+━━━━━┒ギリギリ♂ eye！
+┓┏┓┏┓┃キリキリ♂ mind！
+┛┗┛┗┛┃＼○／
+┓┏┓┏┓┃ /
+┛┗┛┗┛┃ノ)
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┃┃┃┃┃┃
+┻┻┻┻┻┻
+*/
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <climits>
+#include <complex>
+#include <fstream>
+#include <cassert>
+#include <cstdio>
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <ctime>
+#include <set>
+#include <map>
+#include <cmath>
+using namespace std;
+#define fr first
+#define sc second
+#define cl clear
+#define BUG puts("here!!!")
+#define W(a) while(a--)
+#define pb(a) push_back(a)
+#define Rint(a) scanf("%d", &a)
+#define Rll(a) scanf("%lld", &a)
+#define Rs(a) scanf("%s", a)
+#define Cin(a) cin >> a
+#define FRead() freopen("in", "r", stdin)
+#define FWrite() freopen("out", "w", stdout)
+#define Rep(i, len) for(int i = 0; i < (len); i++)
+#define For(i, a, len) for(int i = (a); i < (len); i++)
+#define Cls(a) memset((a), 0, sizeof(a))
+#define Clr(a, x) memset((a), (x), sizeof(a))
+#define Full(a) memset((a), 0x7f7f7f, sizeof(a))
+#define lrt rt << 1
+#define rrt rt << 1 | 1
+#define pi 3.14159265359
+#define RT return
+#define lowbit(x) x & (-x)
+#define onenum(x) __builtin_popcount(x)
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ULL;
+typedef pair<int, int> pii;
+typedef pair<string, int> psi;
+typedef pair<LL, LL> pll;
+typedef map<string, int> msi;
+typedef vector<int> vi;
+typedef vector<LL> vl;
+typedef vector<vl> vvl;
+typedef vector<bool> vb;
+
+const int maxn = 50050;
+LL n, k;
+LL a[maxn], b[maxn];
+
+bool ok(LL x) {
+	LL j = n;
+	LL ret = 0;
+	For(i, 1, n+1) {
+		while(j) {
+			if(a[i] * b[j] > x) j--;
+			else break;
+		}
+		ret += j;
+	}
+	return ret >= k;
+}
+
+int main() {
+	// FRead();
+	while(cin >> n >> k) {
+		LL l = 0, r = 0;
+		k = n * n - k + 1;
+		For(i, 1, n+1) {
+			cin >> a[i] >> b[i];
+		}
+		sort(a+1, a+n+1);
+		sort(b+1, b+n+1);
+		r = a[n] * b[n];
+		LL ret;
+		while(l <= r) {
+			LL m = (l + r) >> 1;
+			if(ok(m)) {
+				ret = m;
+				r = m - 1;
+			}
+			else l = m + 1;
+		}
+		cout << ret << endl;
+	}
+	RT 0;
+}
