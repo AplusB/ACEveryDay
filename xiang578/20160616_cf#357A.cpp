6eed5bfca757d0eeb3b249d5148524c56e813528
Rick Xiang <xiang578@foxmail.com>
2016-06-16 15:28:35 +0800

    Create 20160616_cf#357A.cpp

---
+++ xiang578/20160616_cf#357A.cpp
@@ -0,0 +1,46 @@
+/* ***********************************************
+Author        :Maltub
+Email         :xiang578@foxmail.com
+Blog          :htttp://www.xiang578.com
+************************************************ */
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <string>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+//#include <bits/stdc++.h>
+#define rep(i,a,n) for(int i=a;i<n;i++)
+#define per(i,a,n) for(int i=n-1;i>=a;i--)
+#define pb push_back
+using namespace std;
+typedef vector<int> VI;
+typedef long long ll;
+const ll mod=1000000007;
+const int N=2048;
+
+
+int main()
+{
+    //freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+	int n,a,b;
+	char s[100];
+	int f=0;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		scanf("%s%d%d",s,&a,&b);
+		if(a>=2400&&b>a) f=1;
+	}
+	if(f==1) puts("YES");
+	else puts("NO");
+    return 0;
+}
