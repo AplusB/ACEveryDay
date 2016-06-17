22081f3d617e6cd78c1ca26f9cabee1dbcc16e78
Rick Xiang <xiang578@foxmail.com>
2016-06-17 15:53:09 +0800

    Create 20160617_cfEDU13A.cpp

---
+++ xiang578/20160617_cfEDU13A.cpp
@@ -0,0 +1,39 @@
+/* ***********************************************
+Author        :Maltub
+Email         :xiang578@foxmail.com
+Blog          :htttp://www.xiang578.com
+************************************************ */
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <string>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+//#include <bits/stdc++.h>
+#define rep(i,a,n) for(int i=a;i<n;i++)
+#define per(i,a,n) for(int i=n-1;i>=a;i--)
+#define pb push_back
+using namespace std;
+typedef vector<int> VI;
+typedef long long ll;
+const ll mod=1000000007;
+const int N=2048;
+
+
+int main()
+{
+    //freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+	ll a,n,k;
+	cin>>n>>k;
+	a=n/k;
+	cout<<(a+1)*k;
+	return 0;
+}
