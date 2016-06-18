71ae91e868172d8baf6443e285641e861207c083
eggeek <zsz921130@gmail.com>
2016-06-19 09:50:02 +1000

    haskell a+b

---
+++ eggeek/haskell/list-len.hs
@@ -0,0 +1,6 @@
+--Only fill up the blanks for the function named len
+--Do not modify the structure of the template in any other way
+len :: [a] -> Int
+len lst
+  | null lst = 0
+  | otherwise = 1 + len (tail lst)
