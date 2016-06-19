6c4829a37071899d7c699bbce46a5f46fcc99c93
Snickeen <ff@snickeen.com>
2016-06-19 10:28:44 +0800

    Update 201606197_LightOJ1077.cpp

--- Snickeen/201606197_LightOJ1077.cpp
+++ Snickeen/201606197_LightOJ1077.cpp
@@ -4,11 +4,11 @@ typedef long long LL;
 LL gcd(LL a,LL b){return b==0?a:gcd(b,a%b);}
 int main()
 {
-    	LL t,cas=0,a,b,c,d;
-    	scanf("%lld",&t);
-    	while(t--){
-        		scanf("%lld%lld%lld%lld",&a,&b,&c,&d);
-        		printf("Case %lld: %lld\n",++cas,(LL)__gcd(abs(c-a),abs(d-b))+1LL);
-    	}
-    	return 0;
+	LL t,cas=0,a,b,c,d;
+	scanf("%lld",&t);
+	while(t--){
+		scanf("%lld%lld%lld%lld",&a,&b,&c,&d);
+		printf("Case %lld: %lld\n",++cas,(LL)__gcd(abs(c-a),abs(d-b))+1LL);
+	}
+	return 0;
 }
