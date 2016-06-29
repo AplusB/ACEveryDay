b35e4a9fdb5348490e3ef29281ecb19833b6c251
Kirai <kirai@girigiri.love>
2016-06-29 10:08:37 +0800

    Create 16_06_29_LintCode_perfect-squares.java

---
+++ Kirai/16_06_29_LintCode_perfect-squares.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        for(int i=1; i<=n; i++) dp[i] = i;
+        for(int i=2; i<=n; i++) {
+            for(int j = 1; j*j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i-j*j]+1);
+            }
+        }
+        return dp[n];
+    }
+}
