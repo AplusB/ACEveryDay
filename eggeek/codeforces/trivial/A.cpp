e13eec2847d8408c6835c63cd38b3ec8309b09b5
eggeek <zsz921130@gmail.com>
2016-06-30 01:56:22 +1000

    cf a+b

---
+++ eggeek/codeforces/trivial/A.cpp
@@ -0,0 +1,66 @@
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+#include <cstdarg>
+#include <cstdio>
+#include <random>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <complex>
+#include <numeric>
+#include <limits>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <map>
+#include <set>
+using namespace std;
+#define ALL(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define MP(x, y) make_pair((x),(y))
+#define FI first
+#define SE second
+#define LOWB(x) (x & (-x))
+#define UNIQUE(a) sort(ALL(a)), (a).erase(unique(ALL(a)), (a).end())
+#define HEIGHT(n) (sizeof(int) * 8 - __builtin_clz(n)) //height of range n segment tree
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+template<class T> inline T min(T a, T b, T c) {return min(min(a,b),c);}
+template<class T> inline T min(T a, T b, T c, T d) {return min(min(a,b),min(c,d));}
+template<class T> inline T max(T a, T b, T c) {return max(max(a,b),c);}
+template<class T> inline T max(T a, T b, T c, T d) {return max(max(a,b),max(c,d));}
+const int INF = 1e9;
+const ll INF_LL = 4e18;
+const double pi = acos(-1.0);
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+/*-----------------------------------*/
+string s;
+
+void init() {
+  s = "";
+  int i=1;
+  while (s.length() < 1000) {
+    s += to_string(i);
+    i++;
+  }
+}
+
+int main() {
+  // freopen("test.in", "r", stdin);
+  init();
+  int n;
+  cin >> n;
+  cout << s[n-1] << endl;
+  return 0;
+}
