d00ea60d98f2b59daaa604f105aef727df5cfe69
Kirai <kirai@girigiri.love>
2016-07-01 13:29:15 +0800

    Create 16_07_01_HIHO_1323.cpp

---
+++ Kirai/16_07_01_HIHO_1323.cpp
@@ -0,0 +1,95 @@
+	/*
+━━━━━┒ギリギリ♂ eye！
+┓┏┓┏┓┃キリキリ♂ mind！
+┛┗┛┗┛┃＼○／
+┓┏┓┏┓┃ /
+┛┗┛┗┛┃ノ)
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┃┃┃┃┃┃
+┻┻┻┻┻┻
+*/
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <climits>
+#include <complex>
+#include <fstream>
+#include <cassert>
+#include <cstdio>
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <ctime>
+#include <set>
+#include <map>
+#include <cmath>
+using namespace std;
+#define fr first
+#define sc second
+#define cl clear
+#define BUG puts("here!!!")
+#define W(a) while(a--)
+#define pb(a) push_back(a)
+#define Rint(a) scanf("%d", &a)
+#define Rll(a) scanf("%lld", &a)
+#define Rs(a) scanf("%s", a)
+#define Cin(a) cin >> a
+#define FRead() freopen("in", "r", stdin)
+#define FWrite() freopen("out", "w", stdout)
+#define Rep(i, len) for(int i = 0; i < (len); i++)
+#define For(i, a, len) for(int i = (a); i < (len); i++)
+#define Cls(a) memset((a), 0, sizeof(a))
+#define Clr(a, x) memset((a), (x), sizeof(a))
+#define Full(a) memset((a), 0x7f7f7f, sizeof(a))
+#define lrt rt << 1
+#define rrt rt << 1 | 1
+#define pi 3.14159265359
+#define RT return
+#define lowbit(x) x & (-x)
+#define onenum(x) __builtin_popcount(x)
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ULL;
+typedef pair<int, int> pii;
+typedef pair<string, int> psi;
+typedef pair<LL, LL> pll;
+typedef map<string, int> msi;
+typedef vector<int> vi;
+typedef vector<LL> vl;
+typedef vector<vl> vvl;
+typedef vector<bool> vb;
+
+const int maxn = 110;
+
+int dp[maxn][maxn];
+char s[maxn];
+int n;
+
+int main() {
+	// FRead();
+	while(~Rs(s+1)) {
+		n = strlen(s+1);
+		Cls(dp);
+		For(k, 1, n+1) {
+			For(i, 1, n-k+1) {
+				int j = i + k;
+				dp[i][j] = n;
+				if(s[i] == s[j]) dp[i][j] = min(dp[i][j], dp[i+1][j-1]);
+				dp[i][j] = min(dp[i+1][j]+1, dp[i][j]);
+				dp[i][j] = min(dp[i][j-1]+1, dp[i][j]);
+				dp[i][j] = min(dp[i+1][j-1]+1, dp[i][j]);
+			}
+		}
+		printf("%d\n", dp[1][n]);
+	}
+	RT 0;
+}
