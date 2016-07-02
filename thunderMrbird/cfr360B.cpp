63c57a611a21241c781707e9337e584c2f051a7a
thunderMrbird <598786111@qq.com>
2016-07-02 19:12:10 +0800

    Create cfr360B.cpp

---
+++ thunderMrbird/cfr360B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <stack>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+typedef long long ll;
+int main()
+{
+       string s;
+       cin>>s;
+       cout<<s;
+       reverse(s.begin(),s.end());
+       cout<<s<<endl;
+}
