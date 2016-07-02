03ab4637b02e7c90e73a45442bd7d3545d732d0e
yunmagz <787930982@qq.com>
2016-07-02 19:16:05 +0800

    Update 2016_07_02.cpp

--- yunmagz/2016_07_02.cpp
+++ yunmagz/2016_07_02.cpp
@@ -53,3 +53,39 @@ int main(){
     }
     return 0;
 }
+//lightoj1170 - Counting Perfect BST
+#include<bits/stdc++.h>
+using namespace std;
+#define LL long long
+const int mod=1e8+7;
+const double eps=1e-9;
+LL dp[100010];
+int cnt=0,m[100010],p[100010];
+int main(){
+    memset(m,0,sizeof m);
+    for(LL i=2;i<=100000;i++){
+        if(!m[i]){
+            p[cnt++]=i;
+            for(LL j=i*i;j<=100000;j*=i) m[j]=1;
+        }
+    }
+    dp[0]=1;
+    for(int i=1; i<5000; i++)
+        for(int j=0; j<i; j++)
+            dp[i]=(dp[i]+dp[j]*dp[i-1-j])%mod;
+    dp[0]=0;
+    int t,cas=1,x;
+    cin>>t;
+    while(t--){
+        LL a,b;
+        cin>>a>>b;
+        LL ans=0,x,y;
+        for(int i=2;i<34;i++){
+            x=(int)floor(pow(a-1,1.0/i)+eps);
+            y=(int)floor(pow(b,1.0/i)+eps);
+            ans+=(upper_bound(p,p+cnt,y)-p)-(upper_bound(p,p+cnt,x)-p);
+        }
+        printf("Case %d: %lld\n",cas++,dp[ans]);
+    }
+    return 0;
+}
