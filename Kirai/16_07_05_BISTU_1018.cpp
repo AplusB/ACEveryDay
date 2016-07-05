b98bf767009a2d52974367cc37bf8073bedef5fa
Kirai <kirai@girigiri.love>
2016-07-05 10:05:09 +0800

    Create 16_07_05_BISTU_1018.cpp

---
+++ Kirai/16_07_05_BISTU_1018.cpp
@@ -0,0 +1,93 @@
+/*
+━━━━━┒ギリギリ♂ eye！
+┓┏┓┏┓┃キリキリ♂ mind！
+┛┗┛┗┛┃＼○／
+┓┏┓┏┓┃ /
+┛┗┛┗┛┃ノ)
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┛┗┛┗┛┃
+┓┏┓┏┓┃
+┃┃┃┃┃┃
+┻┻┻┻┻┻
+*/
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <climits>
+#include <complex>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <ctime>
+#include <set>
+#include <map>
+#include <cmath>
+using namespace std;
+#define fr first
+#define sc second
+#define cl clear
+#define BUG puts("here!!!")
+#define W(a) while(a--)
+#define pb(a) push_back(a)
+#define Rint(a) scanf("%d", &a)
+#define Rll(a) scanf("%I64d", &a)
+#define Rs(a) scanf("%s", a)
+#define Cin(a) cin >> a
+#define FRead() freopen("in", "r", stdin)
+#define FWrite() freopen("out", "w", stdout)
+#define Rep(i, len) for(int i = 0; i < (len); i++)
+#define For(i, a, len) for(int i = (a); i < (len); i++)
+#define Cls(a) memset((a), 0, sizeof(a))
+#define Clr(a, x) memset((a), (x), sizeof(a))
+#define Full(a) memset((a), 0x7f7f7f, sizeof(a))
+#define lrt rt << 1
+#define rrt rt << 1 | 1
+#define pi 3.14159265359
+#define RT return
+#define lowbit(x) x & (-x)
+#define onenum(x) __builtin_popcount(x)
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ULL;
+typedef pair<int, int> pii;
+typedef pair<string, int> psi;
+typedef pair<LL, LL> pll;
+typedef map<string, int> msi;
+typedef vector<int> vi;
+typedef vector<LL> vl;
+typedef vector<vl> vvl;
+typedef vector<bool> vb;
+
+const int maxn = 100010;
+int n;
+int a[maxn];
+bool check(){
+	Rep(i, n-1) {
+		if(a[i] > a[i+1]) return 0;
+	}
+	return 1;
+}
+
+int main() {
+	// FRead();
+    srand(time(0));
+    int T;
+    Rint(T);
+    W(T) {
+    	Rint(n);
+    	Rep(i, n) Rint(a[i]);
+        while(!check()) std::random_shuffle(a, a+n);
+        printf("%d\n", a[n-1]);
+    }
+    return 0;
+}
