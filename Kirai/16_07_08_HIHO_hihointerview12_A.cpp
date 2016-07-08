b722b628aa6654e30d03a4376fc3750d038f65ed
Kirai <kirai@girigiri.love>
2016-07-08 12:51:44 +0800

    Create 16_07_08_HIHO_hihointerview12_A.cpp

---
+++ Kirai/16_07_08_HIHO_hihointerview12_A.cpp
@@ -0,0 +1,88 @@
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <climits>
+#include <complex>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <bitset>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <ctime>
+#include <set>
+#include <map>
+#include <cmath>
+using namespace std;
+#define fr first
+#define sc second
+#define cl clear
+#define BUG puts("here!!!")
+#define W(a) while(a--)
+#define pb(a) push_back(a)
+#define Rint(a) scanf("%d", &a)
+#define Rll(a) scanf("%I64d", &a)
+#define Rs(a) scanf("%s", a)
+#define Cin(a) cin >> a
+#define FRead() freopen("in", "r", stdin)
+#define FWrite() freopen("out", "w", stdout)
+#define Rep(i, len) for(int i = 0; i < (len); i++)
+#define For(i, a, len) for(int i = (a); i < (len); i++)
+#define Cls(a) memset((a), 0, sizeof(a))
+#define Clr(a, x) memset((a), (x), sizeof(a))
+#define Full(a) memset((a), 0x7f7f7f, sizeof(a))
+#define lrt rt << 1
+#define rrt rt << 1 | 1
+#define pi 3.14159265359
+#define RT return
+#define lowbit(x) x & (-x)
+#define onenum(x) __builtin_popcount(x)
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ULL;
+typedef pair<int, int> pii;
+typedef pair<string, int> psi;
+typedef pair<LL, LL> pll;
+typedef map<string, int> msi;
+typedef vector<int> vi;
+typedef vector<LL> vl;
+typedef vector<vl> vvl;
+typedef vector<bool> vb;
+
+const int maxn = 1000;
+string s[maxn];
+int n;
+int ascii[256];
+
+int main() {
+	// FRead();
+	while(~Rint(n)) {
+		Rep(i, n) cin >> s[i];
+		sort(s, s+n);
+		int ret = 0;
+		Rep(k, n) {
+			Cls(ascii);
+			Rep(j, s[k].length()) ascii[s[k][j]] = 1;
+			int cur = 1;
+			Rep(i, n) {
+				bool flag = 0;
+				Rep(j, s[i].length()) {
+					if(ascii[s[i][j]] == 1) {
+						flag = 1;
+						break;
+					}
+				}
+				if(flag == 0) {
+					Rep(j, s[i].length()) ascii[s[i][j]] = 1;
+					cur++;
+				}
+			}
+			ret = max(ret, cur);
+		}
+		printf("%d\n", ret);
+	}
+	RT 0;
+}
