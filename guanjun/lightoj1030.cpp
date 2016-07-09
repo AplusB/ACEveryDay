3178e09cc773048803a64a0aea9db88e0c84d453
guanjun <guanjunace@gmail.com>
2016-07-09 23:51:15 +0800

    概率dp

---
+++ guanjun/lightoj1030.cpp
@@ -0,0 +1,83 @@
+/* ***********************************************
+Author        :guanjun
+Created Time  :2016/7/9 23:17:57
+File Name     :1030.cpp
+************************************************ */
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <string>
+#include <math.h>
+#include <stdlib.h>
+#include <iomanip>
+#include <list>
+#include <deque>
+#include <stack>
+#define ull unsigned long long
+#define ll long long
+#define mod 90001
+#define INF 0x3f3f3f3f
+#define maxn 10010
+#define cle(a) memset(a,0,sizeof(a))
+const ull inf = 1LL << 61;
+const double eps=1e-5;
+using namespace std;
+priority_queue<int,vector<int>,greater<int> >pq;
+struct Node{
+	int x,y;
+};
+struct cmp{
+    bool operator()(Node a,Node b){
+        if(a.x==b.x) return a.y> b.y;
+        return a.x>b.x;
+	}
+};
+
+bool cmp(int a,int b){
+    return a>b;
+}
+int n;
+double a[maxn];
+double dp[maxn];
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt","r",stdin);
+    #endif
+    //freopen("out.txt","w",stdout);
+    int T;
+	cin>>T;
+	for(int t=1;t<=T;t++){
+		cin>>n;
+		for(int i=1;i<=n;i++)scanf("%lf",&a[i]);
+		cle(dp);
+		double ans=0;
+		dp[1]=1;
+		for(int i=1;i<=n;i++){
+			int k=min(i+6,n);
+			for(int j=i+1;j<=k;j++){
+				dp[j]+=dp[i]/(k-i);
+			}
+			ans+=dp[i]*a[i];
+		}
+		/*
+		for(int i=1;i<=n;i++){
+			int k=min(i,6);
+			for(int j=i-k+1;j<=i;j++){
+				if(j>0)dp[i]+=dp[j]/6.0;
+				else{
+					dp[i]+=dp[j]/1.0*(6+j);
+				}
+			}
+			ans+=dp[i]*a[i];
+		}*/
+		printf("Case %d: %.10f\n",t,ans);
+	}
+    return 0;
+}
