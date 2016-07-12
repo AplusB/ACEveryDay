1a7ae75699ea24a58036d2d21cae7cc42bf07f51
Ephemera <helenawang@users.noreply.github.com>
2016-07-12 23:14:51 +0800

    2016-7-12

---
+++ Helena@ssglalw/cf678a.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <assert.h>
+#define FREAD(fn) freopen((fn), "r", stdin)
+#define RINT(vn) scanf("%d", &(vn))
+#define PINT(vb) printf("%d", vb)
+#define RSTR(vn) scanf("%s", (vn))
+#define PSTR(vn) printf("%s", (vn))
+#define CLEAR(A, X) memset(A, X, sizeof(A))
+#define REP(N) for(i=0; i<(N); i++)
+#define REPE(N) for(i=1; i<=(N); i++)
+#define pb(X) push_back(X)
+#define pn() printf("\n")
+using namespace std;
+const int MAX_N = 100005;
+
+int n, k;
+long long x;
+
+int main()
+{
+	RINT(n);
+	RINT(k);
+	x = n/k + 1;
+	x *= k;
+	cout << x;
+	return 0;
+}
