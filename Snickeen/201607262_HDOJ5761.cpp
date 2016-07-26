f3bfc88aa5c72de9b612536c06a02f6561ceeae8
Snickeen <ff@snickeen.com>
2016-07-26 21:44:48 +0800

    Create 201607262_HDOJ5761.cpp

---
+++ Snickeen/201607262_HDOJ5761.cpp
@@ -0,0 +1,79 @@
+//#include<bits/stdc++.h>
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+#include <math.h>
+#include <algorithm>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+#include <utility>
+#include <sstream>
+#include <complex>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <functional>
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef double db;
+typedef long double ldb;
+#define rep(i,a,b) for(i=a;i<b;i++)
+#define repu(i,a,b) for(i=a;i<=b;i++)
+#define repd(i,a,b) for(i=a;i>=b;i--)
+#define repc(i,a,b,c) for(i=a;i<b;i+=c)
+#define repcu(i,a,b,c) for(i=a;i<=b;i+=c)
+#define repcd(i,a,b,c) for(i=a;i>=b;i-=c)
+#define FIN freopen("input.in","r",stdin)
+#define FOUT freopen("output.out","w",stdout)
+#define CC(a) memset(a,0,sizeof(a))
+#define PII pair<int,int>
+#define PLL pair<LL,LL>
+#define VI vector<int>
+#define VII vector<PII>
+#define VLL vector<PLL>
+#define all(x) x.begin(),x.end()
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+
+template<typename T>inline void gn(T &x){
+	x=0;T f=1;char ch=getchar();
+	while(!isdigit(ch)){if(ch=='-')f=-1;ch=getchar();}
+	while(isdigit(ch)){x=x*10+ch-'0';ch=getchar();}
+	x*=f;
+}
+template<typename T>inline void gt(T x){
+	if(!x)return;
+	gt(x/10);putchar(x%10+'0');
+}
+
+int t,n,m,ans,mmax;
+int MOD=1e9+7;
+db fans;
+
+void solve(int a,int v1,int v2){
+	if(v1<v2||(a>0&&v1==v2))puts("Infinity");
+	else printf("%.9lf\n",a*v1*1.0/(v1*v1-v2*v2));
+}
+
+int main()
+{
+	int i,j,k,l,r,v1,v2;
+	while(~scanf("%d %d %d",&n,&v1,&v2)){
+		if(n==0)puts("0");
+		else solve(n,v1,v2);
+	}
+	return 0;
+}
