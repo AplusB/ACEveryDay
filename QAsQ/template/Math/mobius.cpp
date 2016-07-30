736f8a53efa1085e7f056194698873e71be0677f
QAsQ <317661924@qq.com>
2016-07-30 21:12:55 +0800

    update 反演

--- QAsQ/template/Math/mobius.cpp
+++ QAsQ/template/Math/mobius.cpp
@@ -1,19 +1,23 @@
-const int maxn = 10086;
-
-int mobius[maxn];
-
-int main(){
-    memset(mobius,0,sizeof(mobius));
-    for(int i=1;i<maxn;i++){
-        int target = i==1;
-        int delta = target - mobius[i];
-        mobius[i] = delta;
-        for(int j = i * 2;j<maxn;j+=i){
-            mobius[j] += delta;
-        }
+//convert whit mu
+int g[maxn];
+memset(g,0,sizeof(g));
+g[1] = 1;
+for(int i=1;i<=n;i++){
+    for(int j=i+i;j<=n;j+=i){
+        g[j] -= g[i];
     }
-    for(int i=0;i<100;i++)
-        printf("%d ",mobius[i]);
-    return 0;
 }
 
+// sum_n = \sum_1^n mu[i]
+int sum[maxn];
+LL solve(int n,int m)
+{
+    LL ans = 0;
+    if(n > m)swap(n,m);
+    for(int i = 1, last = 0; i <= n; i = last+1)
+    {
+        la = min(n/(n/i),m/(m/i));
+        ans += (LL)(sum[last] - sum[i-1])*(n/i)*(m/i);
+    }
+    return ans;
+}
