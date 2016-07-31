6075cd1f048f8117459e84ceadbf86f3556cca1d
Snickeen <ff@snickeen.com>
2016-07-31 11:06:28 +0800

    Create 201607317_CF702A.cpp

---
+++ Snickeen/201607317_CF702A.cpp
@@ -0,0 +1,93 @@
+//#include<bits/stdc++.h>
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+#include <math.h>
+#include <algorithm>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+#include <utility>
+#include <sstream>
+#include <complex>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <functional>
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef double db;
+typedef long double ldb;
+#define rep(i,a,b) for(i=a;i<b;i++)
+#define repu(i,a,b) for(i=a;i<=b;i++)
+#define repd(i,a,b) for(i=a;i>=b;i--)
+#define repc(i,a,b,c) for(i=a;i<b;i+=c)
+#define repcu(i,a,b,c) for(i=a;i<=b;i+=c)
+#define repcd(i,a,b,c) for(i=a;i>=b;i-=c)
+#define FIN freopen("input.in","r",stdin)
+#define FOUT freopen("output.out","w",stdout)
+#define CC(a) memset(a,0,sizeof(a))
+#define PII pair<int,int>
+#define PLL pair<LL,LL>
+#define VI vector<int>
+#define VII vector<PII>
+#define VLL vector<PLL>
+#define all(x) x.begin(),x.end()
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+
+template<typename T>inline void gn(T &x){
+	x=0;T f=1;char ch=getchar();
+	while(!isdigit(ch)){if(ch=='-')f=-1;ch=getchar();}
+	while(isdigit(ch)){x=x*10+ch-'0';ch=getchar();}
+	x*=f;
+}
+template<typename T>inline void gt(T x){
+	if(!x)return;
+	gt(x/10);putchar(x%10+'0');
+}
+
+struct TT{
+	LL a,b;
+}v[5];
+LL t,n,m,ans,l,r;
+int MOD=1e9+7;
+vector<int> f;
+deque<int> d;
+int ff[100005];
+
+int cmp(TT a,TT b){
+	if(a.a!=b.a)return a.a<b.a;
+	return a.b<b.b;
+}
+
+int main()
+{
+	LL i,j,k,cnt=0;
+	gn(n);
+	repu(i,1,n)gn(ff[i]);
+	ff[0]=MOD;ff[n+1]=-1;
+	repu(i,1,n+1){
+		if(ff[i]<=ff[i-1]){
+			ans=max(ans,cnt);
+			cnt=1;
+		}
+		else {
+			if(ff[i]>ff[i-1])cnt++;
+		}
+	}
+	gt(ans);
+	return 0;
+}
