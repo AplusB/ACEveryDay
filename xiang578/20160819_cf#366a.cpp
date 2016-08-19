4b7a41f514ed4021db18d8c20b3d7190e59a0ed5
Rick Xiang <xiang578@foxmail.com>
2016-08-19 17:04:43 +0800

    Create 20160819_cf#366a.cpp

---
+++ xiang578/20160819_cf#366a.cpp
@@ -0,0 +1,44 @@
+/* ***********************************************
+Author        :Maltub
+Email         :xiang578@foxmail.com
+Blog          :htttp://www.xiang578.com
+************************************************ */
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <string>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+//#include <bits/stdc++.h>
+#define rep(i,a,n) for(int i=a;i<n;i++)
+#define per(i,a,n) for(int i=n-1;i>=a;i--)
+#define pb push_back
+using namespace std;
+typedef vector<int> VI;
+typedef long long ll;
+const ll mod=1000000007;
+const int N=2048;
+
+
+int main()
+{
+    //freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        if(i==1) printf("I hate");
+        else if(i%2==0) printf(" that I love");
+        else printf(" that I hate");
+    }
+    printf(" it\n");
+    return 0;
+}
