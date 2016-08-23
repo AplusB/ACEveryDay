a9a4feb1237c38f22a253d12ecb1e54df10931a5
guanjun <guanjunace@gmail.com>
2016-08-23 22:05:16 +0800

    签到

---
+++ guanjun/1001.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <stdlib.h>
+using namespace std;
+int main()
+{
+	freopen("in.txt","w",stdout);
+	for(int i=1;i<=400;i++)
+	{
+		cout<<i<<endl;
+	}
+	return 0;
+}
