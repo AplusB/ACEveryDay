e942f93ac02d3b165e587143adfbaad49138bfd8
eggeek <zsz921130@gmail.com>
2016-09-03 22:12:10 +1000

    pass cf 625c

---
+++ eggeek/codeforces/625C.cpp
@@ -0,0 +1,77 @@
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+#include <cstdarg>
+#include <cstdio>
+#include <random>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <complex>
+#include <numeric>
+#include <limits>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <map>
+#include <set>
+using namespace std;
+#define ALL(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define MP(x, y) make_pair((x),(y))
+#define FI first
+#define SE second
+#define LOWB(x) (x & (-x))
+#define UNIQUE(a) sort(ALL(a)), (a).erase(unique(ALL(a)), (a).end())
+#define HEIGHT(n) (sizeof(int) * 8 - __builtin_clz(n)) //height of range n segment tree
+#define INF 1e9
+#define INF_LL 4e18
+#define rep(i,a,b) for(__typeof(b) i=a; i<(b); ++i)
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+/*-----------------------------------*/
+
+void print_arr(vector<vi>& arr, int n) {
+  for (int i=1; i<=n; i++) {
+    for (int j=1; j<=n; j++) printf("%d%c", arr[i][j], j+1<=n?' ': '\n');
+  }
+}
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  vi use(n*n+1, 0);
+  vector<vi> a(n+1, vi(n+1, 0));
+  int cur = n*n;
+  for (int i=n; i>=1; i--) {
+    for (int j=n; j>=k; j--) {
+      a[i][j] = cur;
+      use[cur--] = true;
+    }
+  }
+  int val = 1;
+  for (int i=1; i<=n; i++) {
+    for (int j=1; j<k; j++) {
+      while (use[val]) val++;
+      a[i][j] = val;
+      use[val] = true;
+    }
+  }
+  int sum = 0;
+  for (int i=1; i<=n; i++) sum += a[i][k];
+  printf("%d\n", sum);
+  print_arr(a, n);
+  return 0;
+}
