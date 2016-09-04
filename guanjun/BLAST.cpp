f6e2a098ea39870fc7fb5a6ca2dd4db99bcea5bf
guanjun <guanjunace@gmail.com>
2016-09-04 19:25:00 +0800

    dp

---
+++ guanjun/BLAST.cpp
@@ -0,0 +1,67 @@
+/* ***********************************************
+Author        :guanjun
+Created Time  :2016/9/4 18:45:31
+File Name     :1001.cpp
+************************************************ */
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <string>
+#include <math.h>
+#include <stdlib.h>
+#include <iomanip>
+#include <list>
+#include <deque>
+#include <stack>
+#define ull unsigned long long
+#define ll long long
+#define mod 90001
+#define INF 0x3f3f3f3f
+#define maxn 10010
+#define cle(a) memset(a,0,sizeof(a))
+const ull inf = 1LL << 61;
+const double eps=1e-5;
+using namespace std;
+priority_queue<int,vector<int>,greater<int> >pq;
+struct Node{
+	int x,y;
+};
+struct cmp{
+    bool operator()(Node a,Node b){
+        if(a.x==b.x) return a.y> b.y;
+        return a.x>b.x;
+	}
+};
+
+bool cmp(int a,int b){
+    return a>b;
+}
+char s[2100],t[2100];
+int dp[2100][2100];
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt","r",stdin);
+    #endif
+    //freopen("out.txt","w",stdout);
+	int k;
+	scanf("%s %s %d",s+1,t+1,&k);
+		int n=strlen(s+1);
+		int m=strlen(t+1);
+		//dp[0][0]=0;
+		for(int i=0;i<=n;i++)dp[i][0]=k*i;
+		for(int i=0;i<=m;i++)dp[0][i]=k*i;
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=m;j++){
+				dp[i][j]=min(dp[i-1][j-1]+abs(s[i]-t[j]),min(dp[i-1][j],dp[i][j-1])+k);
+			}
+		}
+		cout<<dp[n][m]<<endl;
+    return 0;
+}
