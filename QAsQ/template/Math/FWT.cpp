603abf25439b8d190346b18fe2ea6802378dad2a
QAsQ <317661924@qq.com>
2016-10-02 23:36:23 +0800

    add FWT

---
+++ QAsQ/template/Math/FWT.cpp
@@ -0,0 +1,38 @@
+<TeX> $C_i=\sum_{j \oplus k=i}A_j*B_k$ </TeX>
+void fwt (LL a[] , int n ) {
+    for ( int d = 1 ; d < n ; d <<= 1 ) {
+        for ( int k = d << 1 , i = 0 ; i < n ; i += k ) {
+            for ( int j = 0 ; j < d ; ++ j ) {
+                LL x = a[i + j] , y = a[i + j + d] ;
+                //xor
+                a[i + j] = ( x + y ) % mod ;
+                a[i + j + d] = ( x - y + mod ) % mod ;
+                //and
+                a[i + j] = ( x + y ) % mod;
+                a[i + j + d];
+                //or
+                a[i + j]; 
+                a[i + j + d] = ( y + x ) % mod ;
+            }
+        }
+    }
+}
+
+void ufwt (LL a[] , int n ) {
+    for ( int d = 1 ; d < n ; d <<= 1 ) {
+        for ( int k = d << 1 , i = 0 ; i < n ; i += k ) {
+            for ( int j = 0 ; j < d ; ++ j ) {
+                LL x = a[i + j] , y = a[i + j + d] ;
+                //xor
+                a[i + j] = ( x + y ) * rev_2% mod ;
+                a[i + j + d] = ( x - y + mod ) * rev_2% mod ;
+                //and
+                a[i + j] = ( x - y + mod ) % mod;
+                a[i + j + d];
+                //or  
+                a[i + j]; 
+                a[i + j + d] = ( y - x + mod ) % mod ;
+            }
+        }
+    }
+}
