3793aa106f8b70ea87ed409c59101d535f53b374
haihongs <shh_name@hotmail.com>
2016-10-08 21:37:57 +0800

    Create 2016_10_08.cpp

---
+++ haihongS/2016_10_08.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+
+using namespace std;
+typedef long long ll;
+const int maxn=50+2;
+const int inf=2e9+9;
+const ll mod=1e9+7;
+const double eps=1e-9;
+
+
+int main()
+{
+	ll n;
+	while(~scanf("%lld",&n))
+	{
+	
+		ll sum=(1+n)*n/2;
+		printf("%lld\n\n",sum);
+
+	}
+	return 0;
+}
