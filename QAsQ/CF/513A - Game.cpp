e0587095897492aca9f31ac1205c36d1084a150a
QAsQ <317661924@qq.com>
2016-10-10 17:56:37 +0800

    水题

---
+++ QAsQ/CF/513A - Game.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n1,n2,k1,k2;
+    cin>>n1>>n2>>k1>>k2;
+    if(n1 > n2){
+        cout<<"First";
+    }
+    else{
+        cout<<"Second";
+    }
+    return 0;
+}
