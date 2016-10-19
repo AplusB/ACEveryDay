deecf9056bbe37ab326712b0d4ad6b89d5610ec4
QAsQ <317661924@qq.com>
2016-10-19 17:37:56 +0800

    bitset压位背包

---
+++ QAsQ/Hdu/5890.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int maxn = 55;
+int var[maxn];
+int n;
+int ans[maxn][maxn][maxn];
+
+bitset<88>dp[10];
+
+bool cal(){
+    int a,b,c;
+    int x,y,z;
+    scanf("%d %d %d",&a,&b,&c);
+    x = max(max(a,b),c);
+    z = min(min(a,b),c);
+    y = a + b + c - x - z;
+    int &anw = ans[x][y][z];
+    if(~anw) return anw;
+    for(int i=0;i<=10;i++) dp[i].reset();
+    dp[0][0] = 1;
+    for(int i=1;i<=n;i++){
+        if(i == x || i == y || i == z || var[i] > 87) continue;
+        for(int j=10;j>=1;j--){
+            dp[j] |= dp[j-1]<<var[i];
+        }
+    }
+    return anw = dp[10][87];
+}
+
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t-- && ~scanf("%d",&n)){
+        memset(ans,-1,sizeof(ans));
+        for(int i=1;i<=n;i++){
+            scanf("%d",&var[i]);
+        }
+        int q;
+        scanf("%d",&q);
+        while(q--){
+            puts(cal()?"Yes":"No");
+        }
+    }
+    return 0;
+}
