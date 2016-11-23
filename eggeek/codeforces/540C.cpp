91b81ae3a6e5fc0d7119a6c8cc3ead8cb76ace83
eggeek <zsz921130@gmail.com>
2016-11-24 00:44:54 +1100

    pass cf 540C

---
+++ eggeek/codeforces/540C.cpp
@@ -0,0 +1,97 @@
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+#include <cstdarg>
+#include <cstdio>
+#include <random>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <complex>
+#include <numeric>
+#include <limits>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <map>
+#include <set>
+using namespace std;
+#define ALL(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define MP(x, y) make_pair((x),(y))
+#define FI first
+#define SE second
+#define LOWB(x) (x & (-x))
+#define UNIQUE(a) sort(ALL(a)), (a).erase(unique(ALL(a)), (a).end())
+#define HEIGHT(n) (sizeof(int) * 8 - __builtin_clz(n)) //height of range n segment tree
+#define INF 1e9
+#define INF_LL 4e18
+#define rep(i,a,b) for(__typeof(b) i=a; i<(b); ++i)
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+/*-----------------------------------*/
+#define N 501
+int n, m, r0, c0, r1, c1;
+char g[N][N];
+bool vis[N][N];
+queue<pii> q;
+bool solve() {
+  q.push({r0, c0});
+  vis[r0][c0] = true;
+  while (!q.empty()) {
+    pii c = q.front(); q.pop();
+    for (int i=0; i<4; i++) {
+      int nxtr = c.first + dx[i];
+      int nxtc = c.second + dy[i];
+      if (nxtr >= n || nxtr < 0 || nxtc >= m || nxtc < 0) continue;
+      if (nxtr == r1 && nxtc == c1) {
+        if (!vis[nxtr][nxtc]) {
+          vis[nxtr][nxtc] = true;
+          q.push({nxtr, nxtc});
+        }
+      } else {
+        if (vis[nxtr][nxtc]) continue;
+        if (g[nxtr][nxtc] == 'X') continue;
+        q.push({nxtr, nxtc});
+        vis[nxtr][nxtc] = true;
+      }
+
+    }
+  }
+  if (!vis[r1][c1]) return false;
+  int cnt = 0;
+  for (int i=0; i<4; i++) {
+    int nxtr = r1 + dx[i];
+    int nxtc = c1 + dy[i];
+    if (nxtr >= n || nxtr < 0 || nxtc >= m || nxtc < 0) continue;
+    if (g[nxtr][nxtc] == '.' || (nxtr == r0 && nxtc == c0)) cnt++;
+  }
+  if (r1 == r0 && c1 == c0) {
+    if (g[r1][c1] == 'X') return cnt >= 1;
+    else return cnt >= 2;
+  } else {
+    if (g[r1][c1] == 'X') return true;
+    else return cnt >= 2;
+  }
+}
+
+int main() {
+  scanf("%d%d", &n, &m);
+  for (int i=0; i<n; i++) scanf("%s", g[i]);
+  scanf("%d%d%d%d", &r0, &c0, &r1, &c1);
+  r0--, c0--, r1--, c1--;
+  printf(solve()?"YES\n": "NO\n");
+  return 0;
+}
