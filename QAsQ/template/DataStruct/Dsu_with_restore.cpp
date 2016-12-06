27d6f1bf84b94e0f6921c64b7c406248a38ebd00
QAsQ <317661924@qq.com>
2016-12-06 17:25:10 +0800

    修改可撤销并查集模板

--- QAsQ/template/DataStruct/Dsu_with_restore.cpp
+++ QAsQ/template/DataStruct/Dsu_with_restore.cpp
@@ -1,40 +1,32 @@
-﻿#include<bits/stdc++.h>
-using namespace std;
-const int maxn = 112345;
-int fa[maxn], ds[maxn], rk[maxn];
-stack<int> S;
-void init(int n) {
-    for (int i = 1; i <= n; ++ i) {
-        fa[i] = i, rk[i] = ds[i] = 0;
-    }
-    while(S.empty()==false) S.pop();
-}
-int dis(int x) {
-    int r(0);
-    for (; x != fa[x]; x = fa[x])
-        r ^= ds[x];
-    return r;
+﻿int fa[maxn],fval[maxn];
+void Init(int n) {
+    for(int i=0;i<=n;i++)
+        fa[i]=i,fval[i]=0;
 }
 int fnd(int x) {
-    while (x != fa[x]) x = fa[x];
-    return fa[x];
+    while(x!=fa[x])x=fa[x];
+    return x;
 }
-void merge(int x, int y, int d) { //x -(d)-> y
-    x = fnd(x); y = fnd(y);
-    if (x == y) return; //ignore
-    if (rk[x] > rk[y])
-        swap(x, y);
-    if (rk[x] == rk[y])
-        ++ rk[y], S.push(-y);
-    fa[x] = y; ds[x] = d;
-    S.push(x);
+stack<pair<int*,int>>S;
+void join(int x,int y,bool on) {
+    x=fnd(x),y=fnd(y);
+    if(x==y)return;
+    if(fval[x]<=fval[y]) {
+        if(on) S.push(make_pair(&fa[x],fa[x]));
+        fa[x]=y;
+        if(fval[x]==fval[y]) {
+            if(on) S.push(make_pair(&fval[y],fval[y]));
+            fval[y]++;
+        }
+    }
+    else {
+        if(on)S.push(make_pair(&fa[y],fa[y]));
+        fa[y]=x;
+    }
 }
-void restore(int ed) { //back to state before ed'th operator
-    while(S.size() > ed) {
-        if (S.top() < 0)
-            --rk[-S.top()];
-        else
-            fa[S.top()] = S.top(), ds[S.top()] = 0;
+void back(bool on) {
+    while(!S.empty()) {
+        if(on) *S.top().first=S.top().second;
         S.pop();
     }
 }
