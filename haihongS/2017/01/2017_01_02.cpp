4faf30568f8457d88634cd3160dc5c2c6fe139c4
haihongs <shh_name@hotmail.com>
2017-01-02 14:05:20 +0800

    Update 2017_01_02.cpp

--- haihongS/2017/01/2017_01_02.cpp
+++ haihongS/2017/01/2017_01_02.cpp
@@ -3,29 +3,101 @@
 using namespace std;
 
 typedef long long ll;
-const int maxn=2000000+60;
+const int maxn=5e2+60;
 const ll inf=2e9+9;
 
-int a[maxn],b[maxn],c[maxn];
+int n,m,pos,st,ed;
+int d[maxn],d2[maxn],ans[maxn];
+
+struct edge
+{
+	int to,w1,w2;
+};
+
+vector<edge> G[maxn];
+
+void add_edge(int u,int v,int w1,int w2);
+void Dijkstra(int s);
+void deal(int v);
 
 int main()
 {
-	int x,y;
-	while(~scanf("%d",&x))
+	while(~scanf("%d%d%d%d",&n,&m,&st,&ed))
 	{
-		for(int i=0;i<x;i++)
-			scanf("%d",&a[i]);
-		scanf("%d",&y);
-		for(int i=0;i<y;i++)
-			scanf("%d",&b[i]);
-		int pos=0;
-		for(int i=0;i<x;i++)
-			c[pos++]=a[i];
-		for(int j=0;j<y;j++)
-			c[pos++]=b[j];
-		sort(c,c+pos);
-		pos=unique(c,c+pos)-c;
-		printf("%d\n",c[(pos-1)/2]);
+		for(int i=0;i<maxn;i++)
+			G[i].clear();
+		pos=0;
+		for(int i=0;i<m;i++)
+		{
+			int u,v,w1,w2;
+			scanf("%d%d%d%d",&u,&v,&w1,&w2);
+			add_edge(u,v,w1,w2);
+			add_edge(v,u,w1,w2);
+		}
+		Dijkstra(st);
+		deal(ed);
+		for(int i=pos-1;i>=0;i--)
+		{
+			if(i!=pos-1) printf(" ");
+			printf("%d",ans[i]);
+		}
+		printf(" %d %d\n",d[ed],d2[ed]);
 	}
 	return 0;
+}
+
+void deal(int v)
+{
+	ans[pos++]=v;
+	if(v==st)	return ;
+	int len=G[v].size();
+	for(int i=0;i<len;i++)
+	{
+		edge nx=G[v][i];
+		if(d[nx.to]+nx.w1==d[v] && d2[nx.to]+nx.w2==d2[v])
+		{
+			deal(nx.to);break;
+		}
+	}
+}
+
+void Dijkstra(int s)
+{
+	for(int i=0;i<n;i++)
+		d[i]=inf,d2[i]=inf;
+	d[s]=0,d2[s]=0;
+	queue<edge> aha;
+	while(!aha.empty()) aha.pop();
+	aha.push((edge){s,0,0});
+	while(!aha.empty())
+	{
+		edge tp=aha.front();aha.pop();
+		int v=tp.to;
+		int len=G[v].size();
+		for(int i=0;i<len;i++)
+		{
+			edge nx=G[v][i];
+			if(d[nx.to]>d[v]+nx.w1)
+			{
+				d[nx.to]=d[v]+nx.w1;
+				d2[nx.to]=d2[v]+nx.w2;
+				aha.push((edge){nx.to,d[nx.to],d2[nx.to]});
+			}
+			else if(d[nx.to]==d[v]+nx.w1)
+			{
+				if(d2[nx.to]>d2[v]+nx.w2)
+				{
+					d[nx.to]=d[v]+nx.w1;
+					d2[nx.to]=d2[v]+nx.w2;
+					aha.push((edge){nx.to,d[nx.to],d2[nx.to]});
+				}
+			}
+		}
+	}
+}
+
+void add_edge(int u,int v,int w1,int w2)
+{
+	edge now={v,w1,w2};
+	G[u].push_back(now);
 }
