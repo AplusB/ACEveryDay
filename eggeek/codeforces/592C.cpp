79bca31a107361c10cac6aa00553a13619fa9d2f
eggeek <eggeek.me@gmail.com>
2017-01-26 22:38:11 +0800

    pass cf 592C

---
+++ eggeek/codeforces/592C.cpp
@@ -0,0 +1,60 @@
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+#include <cstdarg>
+#include <cstdio>
+#include <random>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <complex>
+#include <numeric>
+#include <limits>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <map>
+#include <set>
+using namespace std;
+#define ALL(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define MP(x, y) make_pair((x),(y))
+#define FI first
+#define SE second
+#define LOWB(x) (x & (-x))
+#define UNIQUE(a) sort(ALL(a)), (a).erase(unique(ALL(a)), (a).end())
+#define HEIGHT(n) (sizeof(int) * 8 - __builtin_clz(n)) //height of range n segment tree
+#define INF 1e9
+#define INF_LL 4e18
+#define rep(i,a,b) for(__typeof(b) i=a; i<(b); ++i)
+typedef unsigned long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+/*-----------------------------------*/
+
+int main() {
+  ll t, w, b;
+  cin >> t >> w >> b;
+  ll x = min(w, b);
+  ll lcm = w / __gcd(w, b) * b;
+  ll p = t / lcm * x + min(t % lcm + 1ll, x) - 1ll;
+
+  ll q = t;
+  if (p == 0ll) cout << p << "/" << q << endl;
+  else {
+    ll gcd = __gcd(p, q);
+    cout << p / gcd << "/" << q / gcd << endl;
+  }
+  return 0;
+}
