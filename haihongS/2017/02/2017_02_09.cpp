d4a038ab393bd25665dea366c2d0b169e1ffd9b3
haihongs <shh_name@hotmail.com>
2017-02-09 17:51:59 +0800

    Update 2017_02_09.cpp

--- haihongS/2017/02/2017_02_09.cpp
+++ haihongS/2017/02/2017_02_09.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 typedef long long ll;
-const int maxn=1000+9;
+const int maxn=100+3;
 const ll mod=1e9+7;
 const int inf=2e9+9;
 double eps=1e-9;
@@ -10,12 +10,16 @@ double pi=acos(-1.0);
 
 typedef pair<int,int> P;
 
-string a;
-int n;
+int des,pri;
+int n,m;
 
-int judge(char x);
-int go(int pos);
-void deal(int pos);
+struct mat
+{
+	ll v[maxn][maxn];
+};
+
+mat mul(mat a,mat b);
+mat quick_pow(mat x,int t);
 
 int main()
 {
@@ -23,70 +27,101 @@ int main()
 	scanf("%d",&t);
 	while(t--)
 	{
-		cin>>a;
-		scanf("%d",&n);
-		while(n--)
+		mat tmp,now;
+		for(int i=0;i<maxn;i++)
 		{
-			int len=a.size();
-			int now=go(len-1);
-			if(now==0) a[len-1]++;
-			else deal(len-1);
-			printf("%s\n",a.c_str());
+			for(int j=0;j<maxn;j++)
+			{
+				tmp.v[i][j]=now.v[i][j]=0;
+			}
+		}
+		scanf("%d%d",&n,&m);
+		des=n*m-1,pri=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<m;j++)
+			{
+				char mm[200];
+				scanf("%s",mm);
+				int x[10],y[10];
+				sscanf(mm,"((%d,%d),(%d,%d),(%d,%d),(%d,%d))",
+				&x[0],&y[0],&x[1],&y[1],&x[2],&y[2],&x[3],&y[3]);
+				for(int k=0;k<4;k++)
+				{
+					x[k]--,y[k]--;
+					int u=i*m+j;
+					int v=x[k]*m+y[k];
+					if(u!=des && v!=des)
+						now.v[u][v]=1;
+					tmp.v[u][v]=1;
+				}
+			}
+		}
+		int q;
+		scanf("%d",&q);
+		while(q--)
+		{
+			int p;
+			scanf("%d",&p);
+			if(p==0)
+			{
+				if(m==1 && n==1) printf("True\n");
+				else printf("False\n");
+				continue;
+			}
+			mat go;
+			go=quick_pow(now,p-1);
+			go=mul(go,tmp);
+			int flag=go.v[pri][des];
+			if(flag==0) puts("False");
+			else
+			{
+				int cnt=0;
+				for(int i=pri;i<des;i++)
+				{
+					if(go.v[pri][i]>0)
+					{
+						cnt++;
+					}
+				}
+				if(cnt==0) puts("True");
+				else puts("Maybe");
+			}
 		}
 		printf("\n");
 	}
 	return 0;
 }
 
-int go(int pos)
+mat quick_pow(mat x,int t)
 {
-	for(int i=pos;i>=0;i--)
+	mat ans;
+	memset(ans.v,0,sizeof(ans.v));
+	for(int i=pri;i<=des;i++)
+		ans.v[i][i]=1;
+	while(t>0)
 	{
-		if(judge(a[i])) return 1;
+		if(t&1) ans=mul(ans,x);
+		x=mul(x,x);
+		t>>=1;
 	}
-	return 0;
+	return ans;
 }
 
-void deal(int pos)
+mat mul(mat a,mat b)
 {
-	for(int i=pos;i>=0;i--)
+	mat ans;
+	for(int i=pri;i<=des;i++)
 	{
-		if(judge(a[i]))
+		for(int j=pri;j<=des;j++)
 		{
-			if(a[i]=='z')
+			ans.v[i][j]=0;
+			for(int k=pri;k<=des;k++)
 			{
-				a[i]='a';
-				if(i==0 || go(i-1)==0) 
-					a.insert(i,"a");
-					//a='a'+a;
-				else deal(i-1);
+				ans.v[i][j]+=a.v[i][k]*b.v[k][j];
 			}
-			else if(a[i]=='Z')
-			{
-				a[i]='A';
-				if(i==0 || go(i-1)==0)
-					a.insert(i,"A");
-					//a='A'+a;
-				else deal(i-1);
-			}
-			else if(a[i]=='9')
-			{
-				a[i]='0';
-				if(i==0 || go(i-1)==0) 
-					a.insert(i,"1");
-					//a='1'+a;
-				else deal(i-1);
-			}
-			else a[i]++;
-			return ;
+			if(ans.v[i][j]>0) ans.v[i][j]=1;
 		}
 	}
-}
-
-int judge(char x)
-{
-	if('a'<=x && x<='z') return 1;
-	if('A'<=x && x<='Z') return 1;
-	if('0'<=x && x<='9') return 1;
-	return 0;
+	return ans;
 }
