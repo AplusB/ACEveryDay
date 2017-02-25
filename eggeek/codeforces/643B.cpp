fdc376eebfc05023e2a8a56d232d7dde32bcec29
eggeek <eggeek.me@gmail.com>
2017-02-26 09:59:19 +1100

    pass cf 643B

---
+++ eggeek/codeforces/643B.cpp
@@ -0,0 +1,69 @@
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+#include <cstdarg>
+#include <cstdio>
+#include <random>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <complex>
+#include <numeric>
+#include <limits>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <map>
+#include <set>
+using namespace std;
+#define ALL(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define MP(x, y) make_pair((x),(y))
+#define FI first
+#define SE second
+#define LOWB(x) (x & (-x))
+#define UNIQUE(a) sort(ALL(a)), (a).erase(unique(ALL(a)), (a).end())
+#define HEIGHT(n) (sizeof(int) * 8 - __builtin_clz(n)) //height of range n segment tree
+#define INF 1e9
+#define INF_LL 4e18
+#define rep(i,a,b) for(__typeof(b) i=a; i<(b); ++i)
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+/*-----------------------------------*/
+int n, k, a, b, c, d;
+
+void solve() {
+  if (n >= 5) {
+    if (k >= n + 1) {
+      vi l;
+      for (int i=1; i<=n; i++) if (i != a && i != b && i != c && i != d) {
+        l.push_back(i);
+      }
+      printf("%d %d ", a, c);
+      for (int i: l) printf("%d ", i);
+      printf("%d %d\n", d, b);
+
+      printf("%d %d ", c, a);
+      for (int i: l) printf("%d ", i);
+      printf("%d %d\n", b, d);
+    } else printf("-1\n");
+  } else printf("-1\n");
+}
+
+int main() {
+  cin >> n >> k >> a >> b >> c >> d;
+  solve();
+  return 0;
+}
