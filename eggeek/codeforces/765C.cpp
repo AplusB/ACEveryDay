c359f45fce13fcb5cf2dc2ea644216dda58cdec0
eggeek <eggeek.me@gmail.com>
2017-04-06 12:05:32 +1000

    pass cf 765C

---
+++ eggeek/codeforces/765C.cpp
@@ -0,0 +1,61 @@
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+#include <cstdarg>
+#include <cstdio>
+#include <random>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <complex>
+#include <numeric>
+#include <limits>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <map>
+#include <set>
+using namespace std;
+#define ALL(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define MP(x, y) make_pair((x),(y))
+#define FI first
+#define SE second
+#define LOWB(x) (x & (-x))
+#define UNIQUE(a) sort(ALL(a)), (a).erase(unique(ALL(a)), (a).end())
+#define HEIGHT(n) (sizeof(int) * 8 - __builtin_clz(n)) //height of range n segment tree
+#define INF 1e9
+#define INF_LL 4e18
+#define rep(i,a,b) for(__typeof(b) i=a; i<(b); ++i)
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+/*-----------------------------------*/
+
+ll solve(ll k, ll a, ll b) {
+  if (a < b) swap(a, b);
+  ll cnt = a / k + b / k;
+  if (cnt == 0ll) return -1;
+  if (a % k) {
+    if (b / k > 0) return cnt;
+    else return -1;
+  } else return cnt;
+}
+
+int main() {
+  ll k, a, b;
+  cin >> k >> a >> b;
+  cout << solve(k, a, b) << endl;
+  return 0;
+}
