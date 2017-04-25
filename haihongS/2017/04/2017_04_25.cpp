20ff33a425ba1244c18555268cda8296933dbb77
haihongs <shh_name@hotmail.com>
2017-04-25 22:39:06 +0800

    Create 2017_04_25.cpp

---
+++ haihongS/2017/04/2017_04_25.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <ctime>
+#include <iostream>
+
+using namespace std;
+typedef long long ll;
+
+const int maxn = 2e5 + 9;
+
+char a[maxn];
+int ans[maxn];
+
+int main()
+{
+	int a, b;
+	while (~scanf("%d%d", &a, &b))
+	{
+		printf("%d\n\n", a + b);
+	}
+	return 0;
+}
+
