874d37f833d64339013b142e4ef649f8a42d16af
eggeek <eggeek.me@gmail.com>
2017-06-23 22:28:05 +1000

    pass cf 812C

---
+++ eggeek/codeforces/812C.cpp
@@ -0,0 +1,86 @@
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+#include <cstdarg>
+#include <cstdio>
+#include <random>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <complex>
+#include <numeric>
+#include <limits>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <map>
+#include <set>
+using namespace std;
+#define ALL(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define MP(x, y) make_pair((x),(y))
+#define FI first
+#define SE second
+#define LOWB(x) (x & (-x))
+#define UNIQUE(a) sort(ALL(a)), (a).erase(unique(ALL(a)), (a).end())
+#define HEIGHT(n) (sizeof(int) * 8 - __builtin_clz(n)) //height of range n segment tree
+#define INF 1e9
+#define INF_LL 4e18
+#define rep(i,a,b) for(__typeof(b) i=a; i<(b); ++i)
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+/*-----------------------------------*/
+#define N 100010
+ll n, s, a[N];
+
+bool check(int k) {
+  vector<ll> v;
+  for (int i=0; i<n; i++) {
+    v.push_back(a[i] + (ll)k * (i+1ll));
+  }
+  sort(v.begin(), v.end());
+  ll tot = 0;
+  for (int i=0; i<k && tot <= s; i++) tot += v[i];
+  return tot <= s;
+}
+
+int bsearch() {
+  int l=0, r=n, best=0;
+  while (l <= r) {
+    int mid = (l + r) >> 1;
+    if (check(mid)) {
+      best = mid;
+      l = mid + 1;
+    } else r = mid - 1;
+  }
+  return best;
+}
+
+void solve() {
+  int k = bsearch();
+  vector<ll> v;
+  for (int i=0; i<n; i++) v.push_back(a[i] + (ll)k * (i+1ll));
+  sort(v.begin(), v.end());
+  ll T = 0;
+  for (int i=0; i<k; i++) T += v[i];
+  cout << k << " " << T << endl;
+}
+
+int main() {
+  cin >> n >> s;
+  for (int i=0; i<n; i++) cin >> a[i];
+  solve();
+  return 0;
+}
