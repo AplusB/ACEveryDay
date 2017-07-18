2b8e60a4818d1ec8f30f3416ebe64731584af363
eggeek <eggeek.me@gmail.com>
2017-07-18 23:34:15 +1000

    pass cf 589I

---
+++ eggeek/codeforces/589I.cpp
@@ -0,0 +1,63 @@
+#include <stdexcept>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstring>
+#include <cstdarg>
+#include <cstdio>
+#include <random>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <algorithm>
+#include <complex>
+#include <numeric>
+#include <limits>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#include <array>
+#include <list>
+#include <map>
+#include <set>
+using namespace std;
+#define ALL(a) (a).begin(), (a).end()
+#define SZ(a) int((a).size())
+#define MP(x, y) make_pair((x),(y))
+#define FI first
+#define SE second
+#define LOWB(x) (x & (-x))
+#define UNIQUE(a) sort(ALL(a)), (a).erase(unique(ALL(a)), (a).end())
+#define HEIGHT(n) (sizeof(int) * 8 - __builtin_clz(n)) //height of range n segment tree
+#define INF 1e9
+#define INF_LL 4e18
+#define rep(i,a,b) for(__typeof(b) i=a; i<(b); ++i)
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+/*-----------------------------------*/
+#define N 101
+int n, k, cnt[N];
+vi v;
+
+int main() {
+  cin >> n >> k;
+  for (int i=0; i<n; i++) {
+    int c;
+    cin >> c;
+    cnt[c]++;
+  }
+  int res = 0;
+  int need = n / k;
+  for (int i=1; i<=k; i++) {
+    if (cnt[i] > need) res += cnt[i] - need;
+  }
+  cout << res << endl;
+  return 0;
+}
